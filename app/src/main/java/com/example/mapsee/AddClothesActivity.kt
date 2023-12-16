fba2f21efd3fd1749593c9583364c4fe5218a996
sean <seonwoo21@vt.edu>
2023-12-16 17:01:05 -0500

    ""

--- app/src/main/java/com/example/mapsee/AddClothesActivity.kt
+++ app/src/main/java/com/example/mapsee/AddClothesActivity.kt
@@ -1,9 +1,15 @@
 package com.example.mapsee
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mapsee.databinding.ActivityAddClothesBinding
 
@@ -11,20 +17,45 @@ class AddClothesActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddClothesBinding
 
+    private val requestCameraPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            openCamera()
+        } else {
+            Toast.makeText(this, "Camera permission is required to take photos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private val takePictureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == AppCompatActivity.RESULT_OK && result.data != null) {
+            val imageBitmap = result.data?.extras?.get("data") as Bitmap
+            binding.imageViewClothes.setImageBitmap(imageBitmap)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddClothesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnSaveClothes.setOnClickListener {
-            val clothesName = binding.editTextClothesName.text.toString()
-            val brandName = binding.editTextBrandName.text.toString()
-            val isOuterwear = binding.checkBoxOuterwear.isChecked
-            // Handle other checkboxes similarly
+        binding.btnUploadPicture.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                openCamera()
+            } else {
+                requestCameraPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
 
-            // Add logic to save these details and update your data source
-            // After saving, you may want to finish this activity to return to the previous screen
+        // Your existing save button logic
+        binding.btnSaveClothes.setOnClickListener {
+            // Handle the save logic
+            Toast.makeText(this, "Clothes saved!", Toast.LENGTH_SHORT).show()
             finish()
         }
+    }
+
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        takePictureLauncher.launch(cameraIntent)
     }
 }
