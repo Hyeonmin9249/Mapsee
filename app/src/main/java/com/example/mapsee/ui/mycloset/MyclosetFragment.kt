42ef5f571a6afe668d0069cda709f535306afa09
sean <seonwoo21@vt.edu>
2023-12-15 20:40:37 -0500

    "tab1"

--- app/src/main/java/com/example/mapsee/ui/mycloset/MyclosetFragment.kt
+++ app/src/main/java/com/example/mapsee/ui/mycloset/MyclosetFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.mapsee.databinding.FragmentMyclosetBinding
 
 class MyclosetFragment : Fragment() {
 
     private var _binding: FragmentMyclosetBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +18,33 @@ class MyclosetFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val myclosetViewModel =
-            ViewModelProvider(this).get(MyclosetViewModel::class.java)
-
         _binding = FragmentMyclosetBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
+    }
 
-        val textView: TextView = binding.textMycloset
-        myclosetViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Example item counts
+        val outerwearCount = 5
+        val topsCount = 15
+        val bottomsCount = 10
+        val accessoriesCount = 7
+        val totalItems = outerwearCount + topsCount + bottomsCount + accessoriesCount
+
+        // Update progress bars
+        updateProgressBar(binding.progressBarOuterwear, calculatePercentage(outerwearCount, totalItems))
+        updateProgressBar(binding.progressBarTops, calculatePercentage(topsCount, totalItems))
+        updateProgressBar(binding.progressBarBottoms, calculatePercentage(bottomsCount, totalItems))
+        updateProgressBar(binding.progressBarAccessories, calculatePercentage(accessoriesCount, totalItems))
+    }
+
+    private fun calculatePercentage(itemCount: Int, totalItems: Int): Int {
+        return if (totalItems > 0) (itemCount * 100) / totalItems else 0
+    }
+
+    private fun updateProgressBar(progressBar: ProgressBar, percentage: Int) {
+        progressBar.progress = percentage
     }
 
     override fun onDestroyView() {
